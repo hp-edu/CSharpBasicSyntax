6b800db8a0202c60936ced8111d9e5ac0d738117
Hyukpyo Hong <hhong@pharmacentra.com>
2023-07-05 20:35:42 -0400

    Lesson for 20230725

---
+++ Homework2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+The code should check an array of numbers and print an appropriate message for each number: whether it is positive, negative, or zero. However, the code isn't working as expected.
+*/
+
+int[] numbers = new int[5];
+numbers[0] = -1;
+numbers[1] = -2;
+numbers[2] = 0;
+numbers[3] = 4;
+numbers[4] = -5;
+numbers[5] = 10;
+
+
+foreach (int number in numbers)
+{
+    if (number > 0)
+    {
+        Console.WriteLine($"Number {value} is positive");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine($"Number {value} is negative");
+    }
+    else
+    {
+        Console.WriteLine($"Number {value} is zero");
+    }
+}
+/* 
+Expected output: 
+Number -1 is negative
+Number -2 is negative
+Number 0 is zero
+Number 4 is positive
+Number -5 is negative
+*/
