9a64e0af26040acea96ed852515823682634b655
Hyukpyo Hong <hyukpyohong@gmail.com>
2023-07-26 20:59:09 -0400

    Homework 20230726

---
+++ Homework4/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+// Creating instances of the Book class
+Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+Book book2 = new Book("1984", "George Orwell", 1949);
+
+// Creating an instance of the Library class
+Library library = new Library();
+
+// Adding books to the library
+library.AddBook(book1);
+library.AddBook(book2);
+
+// Printing all book details in the library
+library.PrintAllBooks();
+
+public class Book
+{
+    // Fields
+    private string title;
+    private string author;
+    private int publicationYear;
+
+    // Properties
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+
+    public int PublicationYear
+    {
+        get { return publicationYear; }
+        set
+        {
+            if (value >= 1455)
+                publicationYear = value;
+            else
+                throw new Exception("Publication year must be 1455 or later.");
+        }
+    }
+
+    // Constructor
+    public Book(string title, string author, int publicationYear)
+    {
+        Title = title;
+        Author = author;
+        PublicationYear = publicationYear;
+    }
+
+    // Method
+    public void PrintDetails()
+    {
+        Console.WriteLine("Title: " + Title);
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine(string.Format("Publication Year: {0}", PublicationYear));
+    }
+}
+
+public class Library
+{
+    // Field
+    private List<Book> books;
+
+    // Property
+    public List<Book> Books
+    {
+        get { return books; }
+    }
+
+    // Constructor
+    public Library()
+    {
+        books = new List<Book>();
+    }
+
+    // Method to add a book to the library
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    // Method to print all book details in the library
+    public void PrintAllBooks()
+    {
+        foreach (Book book in books)
+        {
+            book.PrintDetails();
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Homework5/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+// Create new event
+var event1 = new Event
+{
+    EventName = "Some Event for Highschool Volunteer",
+    EventLocation = "Central Park",
+    EventDateTime = new DateTime(2023, 8, 30, 13, 00, 00)
+};
+
+// Create some people
+var person1 = new Person { FullName = "John Doe", IsHost = true };
+var person2 = new Person { FullName = "Jane Smith", IsHost = false };
+
+// Add RSVPs
+event1.AddRSVP(person1);
+event1.AddRSVP(person2);
+
+// Check-in
+event1.CheckIn(person1);
+
+// Print attendees (at this point only John Doe has checked in)
+event1.PrintAttendees();
+
+// Check-out
+event1.CheckOut(person1);
+
+// Print attendees (now John Doe has checked out)
+event1.PrintAttendees();
+
+
+public class Person
+{
+    public string FullName { get; set; }
+    public bool IsHost { get; set; }
+}
+
+public class Event
+{
+    public string EventName { get; }
+    public string EventLocation { get; }
+    public DateTime EventDateTime { get; }
+    public List<Person> RSVPs { get; }
+    public List<EventAttendance> Attendances { get; }
+
+    public Event()
+    {
+        RSVPs = new List<Person>();
+        Attendances = new List<EventAttendance>();
+    }
+
+    public void AddRSVP(Person person)
+    {
+        RSVPs.Add(person);
+    }
+
+    public void CheckIn(Person person)
+    {
+        var attendance = new EventAttendance
+        {
+            Attendee = person,
+            AttendedEvent = this,
+            CheckedInAt = DateTime.Now
+        };
+
+        Attendances.Add(attendance);
+    }
+
+    public void CheckOut(Person person)
+    {
+        var attendance = Attendances.Find(a => a.Attendee == person);
+        if (attendance != null)
+        {
+            attendance.CheckedOutAt = DateTime.Now;
+        }
+    }
+
+    public void PrintAttendees()
+    {
+        foreach (var attendance in Attendances)
+        {
+            string checkedIn = attendance.CheckedInAt.HasValue ?
+                attendance.CheckedInAt.Value.ToString("g") : "Not checked in";
+            string checkedOut = attendance.CheckedOutAt.HasValue ?
+                attendance.CheckedOutAt.Value.ToString("g") : "Not checked out";
+
+            Console.WriteLine($"Attendee: {attendance.Attendee.FullName}, Checked in at: {checkedIn}, Checked out at: {checkedOut}");
+        }
+    }
+}
+
+public class EventAttendance
+{
+    public Person Attendee { get; set; }
+    public Event AttendedEvent { get; set; }
+    public DateTime? CheckedInAt { get; set; }
+    public DateTime? CheckedOutAt { get; set; }
+}
+
